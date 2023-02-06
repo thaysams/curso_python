4edfe827af0f31a2887bb172e3cbd1ebb39272c6
thaysams <thaysa.mendess@gmail.com>
2023-02-06 18:49:41 -0400

    Changes to be committed: new file:   jogo_palavras.py

---
+++ jogo_palavras.py
@@ -0,0 +1,23 @@
+# Jogo de letras que mais apararece
+
+frase = 'Leaving like a father. '\
+        'Running like water'.lower()
+
+
+i = 0
+qtd_mais_vezes = 0
+letra_mais_apareceu = ''
+
+
+while i < len(frase):
+    letra = frase[i]
+    qtde_atual = frase.count(letra)
+    
+    if qtd_mais_vezes < qtde_atual and letra != ' ':
+        qtd_mais_vezes = qtde_atual
+        letra_mais_apareceu = letra
+    
+
+    i += 1
+
+print(f'{letra_mais_apareceu} apareceu {qtd_mais_vezes} vezes, sendo a maior qtde')
